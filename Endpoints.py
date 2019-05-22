a35e25c12f0efbb46b0df14a06e381abe255ffec
dereck05 <dereckescalante@hotmail.com>
2019-05-21 22:45:16 -0600

    pruebas

--- Endpoints.py
+++ Endpoints.py
@@ -4,9 +4,20 @@ import boto3
 from io import *
 from multiprocessing.pool import ThreadPool
 
+import psycopg2
+import os
 
 
 app = Flask(__name__)
+
+DATABASE_URL = os.environ['DATABASE_URL']
+try:
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    print('conectado')
+except:
+    "No se puede conectar"
+
+
 
 
 
