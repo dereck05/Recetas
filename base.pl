befc57d36652636e868acb0fedf8745714e2b39c
dereck05 <dereckescalante@hotmail.com>
2019-05-25 20:50:06 -0600

    recetas

--- base.pl
+++ base.pl
@@ -1,4 +1,5 @@
-comida('arroz con pollo',['hola','soy','juan'],'como',['estas','adios'],['hoy','me','voy']).
-comida('frijol',['hola','soy','pedro'],'italiana',['estoy','wuju'],['pastillas','hongos','voy']).
-comida('pizza',['mi','nom'],'es',['lechiza','perro'],['botox','luis']).
+comida('arroz con pollo',['1 taza de pollo','5 tazas de arroz','chile dulce picado','cebolla picada','sal y pimienta'],'tipico',['Primero, mezcle el arroz y el pollo','Segundo, agregue los olores','Finalmente,agregue sal y pimienta'],['url1','url2','url3']).
+comida('cerdo barbacoa',['1 kg de cerdo','sazonador','1 taza de salsa bbq','aceite','sal y pimienta negra'],'carnes',['Primero, caliente el sarten con el aceite','Segundo, meta el cerdo al sarten','Luego,agregue sal y pimienta','Finalmente,agregue salsa barbacoa'],['url1','url2','url3']).
+comida('Gallo Pinto',['2 tazas de arroz','1 taza de frijoles negros','chile dulce picado','cebolla picada','culantro','salsa lizano'],'tipico',['Primero, mezcle el arroz y los frijoles en sarten','Segundo, agregue encima los olores','Luego,agregue un poco de salsa Lizano','Finalmente,sirva a gusto'],['url1','url2','url3']).
+
 
