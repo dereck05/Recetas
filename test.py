32c09e02c19c93562fc355112014f81b22da08a5
Melanie Bermudez <mebeca19@gmail.com>
2019-05-25 19:21:31 -0600

    funcion buscar por nombre y tipo en test.py

--- test.py
+++ test.py
@@ -22,6 +22,31 @@ def pb():
     y = x[0]['X'][1]
 
     print(y)
+    #tipos
+    tipos=[]
+    t='italiana'
+    cont2=0
+    comidi=list(prolog.query("comida(A,Y,B,W,Z)"))
+    for y in prolog.query("comida(A,Y,"+t+",W,Z)"):
+            n=comidi[cont2]['A']
+            tipos.append(n)
+            cont2+=1
+    print(tipos)
+
+
+
+
+    #imprime receta por nombre
+    nombres=[]
+    cont1=0
+    n='pizza'
+    for receta in prolog.query("comida("+n+",Y,B,W,Z)"):
+            
+            nom=comidi[cont1]['A']
+            nombres.append(n)
+            cont1+=1
+    print(nombres)	
+
 x('frijol')
 
 
