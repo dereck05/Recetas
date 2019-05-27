2e91e17c68431fb90344c40db27d33f69df8b741
dereck05 <dereckescalante@hotmail.com>
2019-05-27 17:04:27 -0600

    pruebillas

--- test.py
+++ test.py
@@ -7,7 +7,8 @@ def x(nombre):
     ssh.connect('40.117.154.143',22,'dereck','Progralenguajes123')
     s3 = boto3.resource('s3')
     file = s3.Object('progralenguajes', 'base.pl').get()['Body'].read().decode().replace('\n', '')
-    entrada, salida, error=ssh.exec_command('python prolog.py' + " '" +nombre + "' " +'"'+file+'"')
+    entrada, salida, error = ssh.exec_command('python getAll.py' + ' "' + file + '" ')
+
     print('Salida:',salida.read().decode())
     print('Error:', error.read().decode())
     ssh.close()
