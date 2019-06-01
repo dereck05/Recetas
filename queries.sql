78be8ddf0935da3e7f4c003587ec529f3bffa701
dereck05 <dereckescalante@hotmail.com>
2019-06-01 11:04:00 -0600

    Finalizacion de proyecto

--- queries.sql
+++ queries.sql
@@ -1,1 +1,7 @@
+--update aws set var1 = 'AKIA4MQWCJJIJZ3PAY4X' where var1 = 'AKIA4MQWCJJIDSE6Y5X3';
+
+--update aws set var2 = 'Tn7s4SeDi0+hxYkbZDlK2XFR0TP+8ie8fU8NHQPx' where var1 = 'AKIA4MQWCJJIJZ3PAY4X'
+
+--select * from aws
+
 select * from usuario
